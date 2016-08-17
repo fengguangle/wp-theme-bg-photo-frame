2d232f86f07759e69de5e16bbf2ce58290c3a94c
Takeshi Kashihara <kashihara_ybb@yahoo.co.jp>
2016-08-17 14:41:37 +0900

    no message

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,8 +13,8 @@ if [[ "master" != "$TRAVIS_BRANCH" ]]; then
 fi
  
 git clone -b dist --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git" dist
-#npm run dist
-#cd dist
+npm run dist
+cd dist
 git add -A
 git commit -m "Update from travis $TRAVIS_COMMIT"
 git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" dist 2> /dev/null
